79555fadab99b52727c35d3539ac6c4811c330ae
jasper <cool.daniel1234@gmail.com>
2019-12-02 19:03:14 +0800

    Initial Commit

---
+++ GameDetail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameDetail
+//
+//  Created by User02 on 2019/12/2.
+//  Copyright © 2019 TF. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
